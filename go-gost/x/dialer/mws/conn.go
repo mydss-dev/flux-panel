f6ef59a60dca0bbb39600049cfb1e34b1ba8f68d
mydss-dev <180769754+mydss-dev@users.noreply.github.com>
2026-09-12 13:48:36 +0800

    fix(mwss): harden mux session lifecycle

--- go-gost/x/dialer/mws/conn.go
+++ go-gost/x/dialer/mws/conn.go
@@ -2,13 +2,15 @@ package mws
 
 import (
 	"net"
+	"time"
 
 	"github.com/go-gost/x/internal/util/mux"
 )
 
 type muxSession struct {
-	conn    net.Conn
-	session *mux.Session
+	conn      net.Conn
+	session   *mux.Session
+	createdAt time.Time
 }
 
 func (session *muxSession) GetConn() (net.Conn, error) {
@@ -20,19 +22,46 @@ func (session *muxSession) Accept() (net.Conn, error) {
 }
 
 func (session *muxSession) Close() error {
-	if session.session == nil {
+	if session == nil {
 		return nil
 	}
-	return session.session.Close()
+
+	var err error
+	if session.session != nil {
+		err = session.session.Close()
+	}
+	if session.conn != nil {
+		_ = session.conn.Close()
+	}
+	return err
 }
 
 func (session *muxSession) IsClosed() bool {
-	if session.session == nil {
+	if session == nil {
 		return true
+	}
+	if session.session == nil {
+		return session.conn == nil
 	}
 	return session.session.IsClosed()
 }
 
+func (session *muxSession) ShouldRotate(now time.Time, maxLifetime time.Duration) bool {
+	if session == nil || maxLifetime <= 0 || session.createdAt.IsZero() {
+		return false
+	}
+	if now.Sub(session.createdAt) < maxLifetime {
+		return false
+	}
+	if session.session == nil {
+		return true
+	}
+	return session.session.NumStreams() == 0
+}
+
 func (session *muxSession) NumStreams() int {
+	if session == nil || session.session == nil {
+		return 0
+	}
 	return session.session.NumStreams()
 }
